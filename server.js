b3974cc1236d1f9e4f968ba6187c4349623ec032
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2025-02-17 22:43:28 +0100

    Added method for logging in

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const db = require("./db");
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -73,6 +76,29 @@ app.post('/register', async (req, res) => {
         res.send({ message: 'User registered successfully', result });
     } catch (error) {
         res.send(error.message);
+    }
+});
+
+app.post('/login', async (req, res) => {
+    try {
+        const { username, userpassword } = req.body;
+        let sql = "SELECT * FROM users WHERE username=?";
+        try {
+            const user = await db.query(sql, [username]);
+            if (!user) {
+                return res.status(401).json({ error: 'Authentication failed' });
+            }
+            const passwordMatch = await bcrypt.compare(userpassword, user[0].userpassword);
+            if (!passwordMatch) {
+                return res.status(401).json({ error: 'Wrong password' });
+            }
+            const token = jwt.sign({ id: user[0].id, name: username }, process.env.TOKEN_SECRET, { expiresIn: '1h', });
+            res.json({ token });
+        } catch (error) {
+            res.status(404).send(error.massage);
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Login failed' });
     }
 });
 
